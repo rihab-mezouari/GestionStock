d60f28ec8d963f26f63dc17ce02466283493caa5
Rihab Mezouari <rihabmezouari1@gmail.com>
2024-05-27 00:51:22 +0200

    gestion client

---
+++ springmvcapp/src/main/java/springmvctp/dao/repositories/ClientRepository.java
@@ -0,0 +1,9 @@
+package springmvctp.dao.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import springmvctp.dao.entities.Client;
+
+public interface ClientRepository extends JpaRepository<Client, Integer> {
+    // Ajoutez une méthode de recherche par email
+    Client findByEmail(String email);
+}
---
+++ springmvcapp/src/main/java/springmvctp/service/serviceimpl/ClientServiceImpl.java
@@ -0,0 +1,47 @@
+package springmvctp.service.serviceimpl;
+
+	import org.springframework.beans.factory.annotation.Autowired;
+	import org.springframework.stereotype.Service;
+	import springmvctp.dao.entities.Client;
+	import springmvctp.dao.repositories.ClientRepository;
+import springmvctp.service.Iservice.clientntService;
+
+import java.util.List;
+	import java.util.Optional;
+
+	@Service
+	public class ClientServiceImpl implements clientntService {
+
+	    @Autowired
+	    private ClientRepository clientRepository;
+
+	    @Override
+	    public void addClient(Client client) {
+	        clientRepository.save(client);
+	    }
+
+	    @Override
+	    public void deleteClient(Integer id) {
+	        clientRepository.deleteById(id);
+	    }
+
+	    @Override
+	    public Client getClient(Integer id) {
+	        Optional<Client> clientOptional = clientRepository.findById(id);
+	        return clientOptional.orElse(null);
+	    }
+
+	    @Override
+	    public void updateClient(Client client) {
+	        clientRepository.save(client);
+	    }
+
+	    @Override
+	    public List<Client> getAllClients() {
+	        return clientRepository.findAll();
+	    }
+	    @Override
+	    public Client getClientByEmail(String email) {
+	        return clientRepository.findByEmail(email);
+	    }
+	}
